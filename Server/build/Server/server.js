0e5d2121a62466d95ff3bb9b3e7fcfcb3db26247
jeffersonnnn <jefferie.halo@gmail.com>
2019-05-20 18:55:19 +0100

    chore(setting up) setting up server environment installing eslint installing babel for transpiling code generating a build file that will contain the ES5 code [Chore: #166114365]

---
+++ Server/build/Server/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('ci with travis');
+});
+
+var server = app.listen(3000, function () {
+  console.log('App running on port 3000');
+});
+
+module.exports = server;
