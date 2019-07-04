dd18340e858cb32fd9aa0fc8959c50cd85220e23
jeffersonnnn <jefferie.halo@gmail.com>
2019-07-04 13:49:05 +0100

    feature(sign in): allow user to sign in with details in database [Feature: #167069962]

--- Server/src/controllers/userController.js
+++ Server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel';
 import JwtAuthenticate from '../utils/generateToken';
+import Password from '../utils/password';
 
 class UserController {
   static async registerUser(req, res) {
@@ -27,10 +28,65 @@ class UserController {
         status: 201,
         data: {
           token,
+          id: newUser[0].id,
           firstName: req.body.firstName,
           lastName: req.body.lastName,
           address: req.body.address,
           email: req.body.email,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        error: 'Server error',
+        message:
+          process.env.NODE_ENV === 'production'
+            ? 'Server down. Please try again later'
+            : error.message,
+      });
+    }
+  }
+
+  static async signIn(req, res) {
+    try {
+      const foundUser = await User.findByEmail(req.body.email);
+
+      if (!foundUser.length) {
+        return res.status(400).json({
+          success: false,
+          message: 'Email or Password incorrect',
+        });
+      }
+
+      const isPasswordValid = await Password.comparePassword(
+        req.body.password, foundUser[0].password,
+      );
+
+      if (!isPasswordValid) {
+        return res.status(400).json({
+          success: false,
+          message: 'Email or Password incorrect',
+        });
+      }
+
+      const {
+        id, firstname, lastname, email,
+      } = foundUser[0];
+
+      const token = JwtAuthenticate({
+        id,
+        firstname,
+        lastname,
+        email,
+      });  
+      return res.status(200).json({
+        status: 200,
+        data: {
+          token,
+          id,
+          firstName: firstname,
+          lastName: lastname,
+          email,
         },
       });
     } catch (error) {
--- Server/src/models/dropTables.js
+++ Server/src/models/dropTables.js
@@ -1,17 +1,20 @@
 
-import pool from './pool';
+import pool from './pool'
 
 const dropUsersTable = () => {
   const queryText = `DROP TABLE IF EXISTS users CASCADE;
                     DROP TABLE IF EXISTS orders CASCADE;
                     DROP TABLE IF EXISTS cars CASCADE;
-                    DROP TABLE IF EXISTS flags CASCADE;`;                            
+                    DROP TABLE IF EXISTS flags CASCADE;`                 
   return pool.connect().then((client) => {
-    client.query(queryText)
-      .then(() => {
+    return client.query(queryText)
+      .then((res) => {
+        console.log(res.rows);
         client.end();
       })
-      .catch(() => {
+      .catch((err) => {
+        console.log(err);
+        client.end();
       });
   });
 };
