4e2e1c80730d0c961b44327c868ac81964ff34ed
jeffersonnnn <jefferie.halo@gmail.com>
2019-07-13 21:39:45 +0100

    feature(get Specific Car): get one car [#167276482]

--- Server/src/routes/getCar.js
+++ Server/src/routes/getCar.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import AdvertController from '../controllers/carController';
-import AdValidate from '../middleware/AdValidate';
+import CarsController from '../controllers/carsController';
+// import AdValidate from '../middleware/AdValidate';
 import verifyAuthToken from '../middleware/verifyAuthToken';
 
 const router = express.Router();
 
-router.get('/car/:carId/', verifyAuthToken, AdValidate.validateCarId, AdvertController.getCarById);
-router.get('/car', AdvertController.getAvailableCars);
+// router.get('/car/:carId/', 
+// verifyAuthToken, 
+// AdValidate.validateCarId,
+ // AdvertController.getCarById
+// );
+router.get('/car/:carId/', verifyAuthToken, CarsController.getSpecificCar);
+// router.get('/car', AdvertController.getAvailableCars);
 
 export default router;
